da5bb8e6892be404b49028cd9799d74226a122fb
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-06-07 11:28:54 +0900

    Initial Commit

---
+++ BurgerKim/BurgerKimApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurgerKimApp.swift
+//  BurgerKim
+//
+//  Created by Jinwook Kim on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BurgerKimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BurgerKim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BurgerKim
+//
+//  Created by Jinwook Kim on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
